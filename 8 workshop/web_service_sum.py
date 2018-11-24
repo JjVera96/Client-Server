66fdd96b09aa676edba15e9d9a97fa20453f521f
jjvera96 <jjvera96@gmail.com>
2018-11-24 12:29:13 -0500

    WebServerJson

--- 8 workshop/web_service_sum.py
+++ 8 workshop/web_service_sum.py
@@ -1,20 +1,17 @@
 from flask import Flask
 from flask import request
-import json
-
+from flask_json import FlaskJSON, json_response
 
 app = Flask(__name__)
+json = FlaskJSON(app)
+json.init_app(app)
 
 @app.route('/sum/<int:operatorone>/<int:operatortwo>', methods=['GET'])
 def sum(operatorone, operatortwo):
 
 	value = operatorone + operatortwo
-	response = {
-		'answer' : value
-	}
-
-	return json.dumps(response)
+	return json_response(data=value, headers_={'X-STATUS': 'ok'})
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=8000)
+	app.run(host='0.0.0.0', port=5000)
 
